2c4a5b4d24cce6c97182c2c7e28109bf00b526a1
AlphaK0307 <keyurpatel1121@gmail.com>
2022-04-02 08:00:39 -0500

    commit

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from enum import unique
 from app import db
 from datetime import datetime
-# from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,12 +11,12 @@ class User(db.Model):
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-#         self.password = generate_password_hash(kwargs['password'])
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.password = generate_password_hash(kwargs['password'])
 
-#     def __repr__(self):
-#         return f"<User|{self.username}>"
+    def __repr__(self):
+        return f"<User|{self.username}>"
     
-#     def check_password(self, password):
-#         return check_password_hash(self.password, password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
