aa4e55eb25d49d79883e20ec3ca5d077eb79698c
AlphaK0307 <keyurpatel1121@gmail.com>
2022-04-02 10:35:07 -0500

    commit

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
+from flask import redirect, render_template, url_for
 from app.forms import SignUpForm
+from app.models import User
 
 @app.route('/')
 def index():
@@ -50,8 +51,9 @@ def signup():
         email=form.email.data
         username=form.username.data
         password=form.password.data
-        # print data
-        print(email,username, password)
+        # create new user instance
+        new_user=User(email=email,username=username, password=password)
+        return redirect(url_for('index'))
     return render_template('signup.html', title=title, form=form)
 
 
