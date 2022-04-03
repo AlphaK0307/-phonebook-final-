7b986ec9cbee5d4e0df4870de5ae7f3e11bc0dbe
AlphaK0307 <keyurpatel1121@gmail.com>
2022-04-02 23:29:20 -0500

    commit

--- app/models.py
+++ app/models.py
@@ -39,9 +39,9 @@ class Post(db.Model):
 
 class Phone(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name=db.Column(db.String(100), unique= True, nullable=False)
-    last_name=db.Column(db.String(100), unique= True, nullable=False)
-    phone_number=db.Column(db.String(100), unique= True, nullable=False)
+    first_name=db.Column(db.String(100), nullable=False)
+    last_name=db.Column(db.String(100), nullable=False)
+    phone_number=db.Column(db.String(100), nullable=False)
     city=db.Column(db.String(15), nullable=False)
     date_created= db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
